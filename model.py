d04858d45227ce9ab1c955ef1539d9d73b3aa334
Hackbright Student <info@hackbrightacademy.com>
2016-05-02 12:59:49 -0700

    Added Rating and Movie Class on model.py

--- model.py
+++ model.py
@@ -24,6 +24,27 @@ class User(db.Model):
     zipcode = db.Column(db.String(15), nullable=True)
 
 
+class Movie(db.Model):
+    """Movie on ratings website."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(64), nullable=True)
+    released_at = db.Column(db.DateTime, nullable=True)
+    imdb_url = db.Column(db.String(100), nullable=True)
+
+class Rating(db.Model):
+    """Rating by user on ratings website."""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer, nullable=True)
+    user_id = db.Column(db.Integer, nullable=True)
+    score = db.Column(db.Integer, nullable=True)
+   
+
 # Put your Movie and Rating model classes here.
 
 
