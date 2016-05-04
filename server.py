35055e1befdec288636aa0a74ffd9d66b9e8a6fe
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 18:00:33 -0700

    Added registration and login and template folder

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,67 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/registration")
+def new_user():
+    """Shows new user registation form."""
+
+    return render_template("registration_form.html")
+
+@app.route("/submit-registration", methods=["POST"])
+def submitted():
+    """Confirms new user has been registered."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+
+
+    if User.query.filter_by(email=email).all():
+        print "Email already registered"
+    else:
+        new_user = User(email=email, 
+                        password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+    return render_template("submitted.html", email= email)
+
+
+
+@app.route("/login-form")
+def log_in():
+    """Shows form for user to log in."""
+
+    return render_template('login-form.html')
+
+
+
+@app.route("/handle-login", methods=["POST"])
+def handle_login():
+    """Action for login form: log a user in"""
+
+    form_email = request.form['email']
+    form_password = request.form['password']
+
+    user = User.query.filter(User.email == form_email).one()
+
+    if form_password == user.password:
+        session[user.user_id] = user.email
+        # flash("Logged in as {}").format(user.email)
+        return redirect('/')
+    else:
+        # flash("Wrong password")
+        return redirect("/login-form")
+
 
 
 if __name__ == "__main__":
